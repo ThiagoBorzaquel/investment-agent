8ba3ec029f016e9a90bbd08c14d33f7884b8006d
Thiago <thiago.borzaquel@hotmail.com>
2026-02-25 13:12:33 -0300

    Adiciona envio Telegram

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+import datetime
+import requests
 from data.market_data import get_stock_data
 from analysis.scoring import value_score
-import datetime
 
+# Lista inicial (depois automatizamos índice completo)
 tickers = [
     "PETR4", "VALE3", "ITUB4", "BBDC4", "BBAS3",
     "WEGE3", "MGLU3", "SUZB3", "ELET3", "RENT3",
@@ -9,28 +12,47 @@ tickers = [
     "JBSS3", "RADL3", "HAPV3", "EQTL3", "RAIL3"
 ]
 
+# Coleta dados
 df = get_stock_data(tickers)
 
 df["Score"] = df.apply(value_score, axis=1)
-
 df = df.sort_values("Score", ascending=False)
 
 top20 = df.head(20)
 
-print("\n=== TOP 20 VALUE ===\n")
-print(top20[["Ticker", "PVP", "PL", "ROE", "Score"]])
-
-# Exportar CSV
+# Salvar CSV
 hoje = datetime.date.today()
 arquivo = f"ranking_value_{hoje}.csv"
 top20.to_csv(arquivo, index=False)
 
-print(f"\nArquivo salvo: {arquivo}")
-
-# Criar resumo texto
-mensagem = f"\n📊 Ranking Value Diário - {hoje}\n\n"
+# Criar resumo
+mensagem = f"📊 Ranking Value Diário - {hoje}\n\n"
 
 for i, row in top20.head(5).iterrows():
-    mensagem += f"{row['Ticker']} | Score: {row['Score']} | PVP: {round(row['PVP'],2)} | PL: {round(row['PL'],2)}\n"
+    mensagem += (
+        f"{row['Ticker']} | Score: {row['Score']} "
+        f"| PVP: {round(row['PVP'],2)} "
+        f"| PL: {round(row['PL'],2)}\n"
+    )
 
-print(mensagem)
+# Enviar Telegram
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if TOKEN and CHAT_ID:
+
+    # Enviar mensagem texto
+    url_msg = f"https://api.telegram.org/bot8714485092:AAEBTxwIFbwoE62tcBWtNSdXa-i5ePTAXvY/sendMessage"
+    requests.post(url_msg, data={
+        "chat_id": id": 146536864,
+        "text": mensagem
+    })
+
+    # Enviar CSV
+    url_doc = f"https://api.telegram.org/bot8714485092:AAEBTxwIFbwoE62tcBWtNSdXa-i5ePTAXvY/sendDocument"
+    with open(arquivo, "rb") as file:
+        requests.post(url_doc, data={
+            "chat_id": id": 146536864
+        }, files={
+            "document": file
+        })
