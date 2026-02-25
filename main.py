61928bdee1ad052be774bdb48553b9b0a0f57357
Thiago <thiago.borzaquel@hotmail.com>
2026-02-25 14:43:50 -0300

    Remove token hardcoded

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from data.market_data import get_stock_data
 from analysis.scoring import value_score
 
-# Lista inicial (depois automatizamos índice completo)
+# Lista inicial
 tickers = [
     "PETR4", "VALE3", "ITUB4", "BBDC4", "BBAS3",
     "WEGE3", "MGLU3", "SUZB3", "ELET3", "RENT3",
@@ -12,51 +12,64 @@ tickers = [
     "JBSS3", "RADL3", "HAPV3", "EQTL3", "RAIL3"
 ]
 
-# Coleta dados
-df = get_stock_data(tickers)
+# =========================
+# COLETA E RANKING
+# =========================
 
+df = get_stock_data(tickers)
 df["Score"] = df.apply(value_score, axis=1)
 df = df.sort_values("Score", ascending=False)
 
 top20 = df.head(20)
 
-# Salvar CSV
 hoje = datetime.date.today()
 arquivo = f"ranking_value_{hoje}.csv"
 top20.to_csv(arquivo, index=False)
 
-# Criar resumo
+# =========================
+# MENSAGEM
+# =========================
+
 mensagem = f"📊 Ranking Value Diário - {hoje}\n\n"
 
-for i, row in top20.head(5).iterrows():
+for _, row in top20.head(5).iterrows():
     mensagem += (
-        f"{row['Ticker']} | Score: {row['Score']} "
-        f"| PVP: {round(row['PVP'],2)} "
-        f"| PL: {round(row['PL'],2)}\n"
+        f"{row['Ticker']} | "
+        f"Score: {row['Score']} | "
+        f"PVP: {round(row['PVP'], 2)} | "
+        f"PL: {round(row['PL'], 2)}\n"
     )
 
-# Enviar Telegram
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+# =========================
+# TELEGRAM
+# =========================
 
 
+TOKEN = os.getenv("8714485092:AAEBTxwIFbwoE62tcBWtNSdXa-i5ePTAXvY")
+CHAT_ID = os.getenv('id": 8714485092')
 
-    # Enviar mensagem texto
-    
-    r1 = requests.post(url_msg, data={
-        
-        "text": mensagem
-    })
+print("TOKEN carregado:", bool(TOKEN))
+print("CHAT_ID carregado:", bool(CHAT_ID))
+
+if TOKEN and CHAT_ID:
+
+    url_msg = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    r1 = requests.post(
+        url_msg,
+        data={
+            "chat_id": CHAT_ID,
+            "text": mensagem
+        }
+    )
     print("Mensagem status:", r1.status_code, r1.text)
 
-    # Enviar CSV
-   
+    url_doc = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
     with open(arquivo, "rb") as file:
-        r2 = requests.post(url_doc, data={
-           
-        }, files={
-            "document": file
-        })
+        r2 = requests.post(
+            url_doc,
+            data={"chat_id": CHAT_ID},
+            files={"document": file}
+        )
     print("Documento status:", r2.status_code, r2.text)
 
 else:
